6de2c0db34915028e0c33ae4561d5b593b39d893
Aleksandr Semenychev <aleksandr.semenychev@myworkflow.ru>
2021-08-18 17:45:46 +0700

    initial

---
+++ README.md
@@ -0,0 +1,22 @@
+# Simple React/Redux weather SPA
+
+## Stack
+
+- React
+- Redux
+- SemanticUI React
+- Webpack
+- Babel
+
+## Development
+
+    yarn serve
+
+## Production build
+
+    yarn build
+
+## Docker
+
+    docker build . -t react-weather
+    docker run react-weather
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path')
+
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+
+
+const isDev = process.env.NODE_ENV === 'development'
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: './src/index.js',
+  devtool: isDev && 'eval',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/env', '@babel/react'],
+              plugins: [
+                isDev && require.resolve('react-refresh/babel'),
+              ].filter(Boolean),
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    isDev && new webpack.HotModuleReplacementPlugin(),
+    isDev && new ReactRefreshWebpackPlugin(),
+  ].filter(Boolean),
+  optimization: !isDev ? {} : undefined
+}
